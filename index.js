03d5c2e76f747dd3a1288c14022430cba1642bf7
Jose Adrian Valentin Rodriguez <43792106+joadva@users.noreply.github.com>
2020-10-13 16:35:11 -0500

    index node

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// All your handlers here...
+app.get('/', (req, res) => {/*...*/})
+app.post('/quotes', (req, res) => {/*...*/})
